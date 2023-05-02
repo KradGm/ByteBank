721e7000a4b8dfd47f7bc9dd5e740dee0dee5fe0
KradGm <enzo.loliveira@hotmail.com>
2023-05-02 17:51:14 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public int numero_Agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoAndre = new ContaCorrente
+            {
+                titular = "André Silva",
+                conta = "1010-x",
+                numero_Agencia = 15,
+                saldo = 100
+            };
+            Console.WriteLine("O saldo da conta do > "+contaDoAndre.titular + " é >" + contaDoAndre.saldo);
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
