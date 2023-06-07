8a05df9980bdbed49e491c392cb008b215cfe179
KradGm <enzo.loliveira@hotmail.com>
2023-06-07 16:07:23 -0300

    commit geral

---
+++ ByteBank/Contas/CurrentAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class CurrentAccount
+{
+        public int agencyNumber;
+        public string account;
+        public double balance = 100;
+
+        public Client holder;
+        /*
+        [Criar classe pois tem muitas responsabilidades]
+        public string holderName;
+        public string holderCPF;
+        public string holderProfession;
+        */
+
+
+        //method
+        public void Deposit(double amount)
+        {
+            this.balance += amount;
+        }
+        /// <summary> Esse codigo serve para sacar um certo valor </summary>
+        /// <param name="amount"> Valor a ser medido </param>
+        /// <returns> Nada </returns>
+        public bool Withdraw(double amount)
+        {
+            if(amount <= balance)
+            {
+                this.balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Transfer(double amount, CurrentAccount targetAccount)
+        {
+            if(balance <= amount)
+            {
+                return false;
+            }
+            else
+            {
+                this.Withdraw(amount);
+                targetAccount.Deposit(amount);
+                return true;
+            }
+        }
+        public void Informations(string holder, int agencyNumber, string account, double balance)
+        {
+            Console.WriteLine("O nome do titular é: " + this.holder + " O numero da agencia dele é: " + this.agencyNumber + " O numero da sua conta é: " + this.account + " O seu saldo é : " + this.balance);
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting;
 
 namespace ByteBank
 {
@@ -6,16 +7,108 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDoAndre = new ContaCorrente
+            //CurrentAccount Novaconta = new CurrentAccount();
+            //CurrentAccount contaDoAndre = new CurrentAccount
+            //{
+            //    holderName = "André Silva",
+            //    account = "1010-x",
+            //    agencyNumber = 15,
+            //    balance = 100
+            //};
+
+            //[COMPARAÇÃO DE DADOS]
+            //CurrentAccount contaDoAndre2 = new CurrentAccount
+            //{
+            //    holderName = "André Silva",
+            //    account = "1010-x",
+            //    agencyNumber = 15,
+            //    balance = 100
+            //};
+            //[NÃO APONTAM PARA O MESMO ENDEREÇO(false)]
+            //Console.WriteLine(contaDoAndre == contaDoAndre2);
+            //contaDoAndre = contaDoAndre2; 
+            //[APONTAM PARA O MESMO ENDEREÇO(true)]
+            //Console.WriteLine(contaDoAndre == contaDoAndre2);
+
+            ///*
+            //[INPUT DE DADOS]
+            //Console.WriteLine("Digite o seu nome:");
+            //string holdername = Console.ReadLine();
+            //Novaconta.holder = holdername;
+            //Console.WriteLine(holdername);
+            //*/
+
+            //Console.WriteLine("O saldo da conta do(a) > "+contaDoAndre.holderName + " é >" + contaDoAndre.balance);
+
+            //CurrentAccount contaDaMaria = new CurrentAccount { holderName = "Maria Souza", account = "1010-5", balance = 350, agencyNumber = 17 };
+
+            //Console.WriteLine("O saldo da conta do(a) > " + contaDaMaria.holderName + " é >" + contaDaMaria.balance);
+
+            //contaDoAndre.Transfer(50, contaDaMaria);
+            //Console.WriteLine("O saldo da conta do(a) > " + contaDoAndre.holderName + " é >" + contaDoAndre.balance);
+            //Console.WriteLine("O saldo da conta do(a) > " + contaDaMaria.holderName + " é >" + contaDaMaria.balance);
+
+
+            ///*
+            //[TESTE]
+            //contaDoAndre.Deposit(100);
+
+            //Console.WriteLine("O saldo da conta do > " + contaDoAndre.holder + " é >" + contaDoAndre.balance);
+
+            //if (contaDoAndre.Withdraw(300) == true)
+            //{
+            //    Console.WriteLine("O saldo da conta do > " + contaDoAndre.holder + " é >" + contaDoAndre.balance);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Saldo insuficiente para o saque");
+            //}
+            //*/
+            //CurrentAccount pedroAccount = new CurrentAccount();
+            //Console.WriteLine(pedroAccount.holderName);
+            //Console.WriteLine(pedroAccount.agencyNumber);
+            //Console.WriteLine(pedroAccount.account);
+            //Console.WriteLine(pedroAccount.balance);
+
+
+            ////double valor = 300;
+            ////double valor2 = valor;
+            ////Console.WriteLine(valor == valor2);
+            //contaDaMaria.Informations(contaDaMaria.holderName, contaDaMaria.agencyNumber, contaDaMaria.account, contaDaMaria.balance);
+
+
+            Client client = new Client();
+            client.name = "Enzo Oliveira";
+            client.cpf = "145.014.396-29";
+            client.profession = "Dev/Programmer";
+
+            CurrentAccount account = new CurrentAccount
             {
-                titular = "André Silva",
-                conta = "1010-x",
-                numero_Agencia = 15,
-                saldo = 100
+                holder = client,
+                account = "1010-X",
+                agencyNumber = 17,
+                balance = 102
             };
-            Console.WriteLine("O saldo da conta do > "+contaDoAndre.titular + " é >" + contaDoAndre.saldo);
+            Console.WriteLine("Nome do cliente: " + account.holder.name);
+            Console.WriteLine("CPF do cliente: " + account.holder.cpf);
+            Console.WriteLine("Profissão do cliente: " + account.holder.profession);
+            Console.WriteLine("Conta do cliente: " + account.account);
+            Console.WriteLine("Agencia do cliente: " + account.agencyNumber);
+            Console.WriteLine("Saldo do cliente: " + account.balance);
+
+
+            CurrentAccount account2 = new CurrentAccount
+            {
+                client.holder = client,
+                account = "1015-X",
+                agencyNumber = 16,
+                balance =200
+            }
+
+
             Console.WriteLine("Tecle enter para sair...");
             Console.ReadLine();
+            
         }
     }
 }
